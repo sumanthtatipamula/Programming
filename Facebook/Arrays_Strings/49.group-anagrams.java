4c562a1e9ebb8e0955a464cd08b28951887d1ff6
Sumanth, Tatipamula <sumanthtatipamula999@gmail.com>
2022-12-04 11:53:53 +0530

    vault backup: 2022-12-04 11:53:53

---
+++ Facebook/Arrays_Strings/49.group-anagrams.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.*;
+
+/*
+ * @lc app=leetcode id=49 lang=java
+ *
+ * [49] Group Anagrams
+ *
+ * https://leetcode.com/problems/group-anagrams/description/
+ *
+ * algorithms
+ * Medium (62.58%)
+ * Likes:    13418
+ * Dislikes: 395
+ * Total Accepted:    1.8M
+ * Total Submissions: 2.6M
+ * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+ *
+ * Given an array of strings strs, group the anagrams together. You can return
+ * the answer in any order.
+ * 
+ * An Anagram is a word or phrase formed by rearranging the letters of a
+ * different word or phrase, typically using all the original letters exactly
+ * once.
+ * 
+ * 
+ * Example 1:
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * Example 2:
+ * Input: strs = [""]
+ * Output: [[""]]
+ * Example 3:
+ * Input: strs = ["a"]
+ * Output: [["a"]]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= strs.length <= 10^4
+ * 0 <= strs[i].length <= 100
+ * strs[i] consists of lowercase English letters.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        return new ArrayList<List<String>>();
+    }
+}
+// @lc code=end
+
