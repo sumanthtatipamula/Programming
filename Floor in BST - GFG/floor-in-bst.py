4eec6a4625defc18ddbaa8ffe6a6863b92ee721a
Sumanth Tatipamula <sumanthtatipamula999@gmail.com>
2023-02-07 09:09:18 +0530

    Added solution - LeetHub

---
+++ Floor in BST - GFG/floor-in-bst.py
@@ -0,0 +1,47 @@
+#User function Template for python3
+
+class Solution:
+    def floor(self, root, x):
+        result = -1
+        while(root):
+            if(root.data == x):
+                return x
+            if(root.data < x):
+                result = root.data
+                root = root.right
+            else:
+                root = root.left
+        return result
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+def insert(tree, val):
+    if(tree==None):
+        return Node(val)
+    if(val< tree.data):
+        tree.left= insert(tree.left, val)
+    elif(val > tree.data):
+        tree.right= insert(tree.right, val)
+    return tree
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        n=int(input())
+        arr= list(map(int, input().split()))
+        root = None
+        for k in arr:
+            root=insert(root, k)
+        s=int(input())
+        obj = Solution()
+        print(obj.floor(root,s))
+# } Driver Code Ends
