bdfd5bfa35c0f463dbb3d57281c38abb1853a46a
Sumanth Tatipamula <sumanthtatipamula999@gmail.com>
2023-04-28 03:51:22 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -4,19 +4,13 @@ class Solution:
     
     #Function to return max value that can be put in knapsack of capacity W.
     def knapSack(self, size, weight, value, n):
-        mem = {}
-        def fillBag(index, remSize):
-            if(index == n):
-                return 0
-            inclusive = 0
-            if((index, remSize) in mem):
-                return mem[(index, remSize)]
-            if(remSize >= weight[index]):
-                inclusive = value[index] + fillBag(index + 1, remSize - weight[index])
-            exclusive = fillBag(index + 1 , remSize)
-            mem[(index,remSize)] = max(inclusive, exclusive)
-            return mem[(index, remSize)]
-        return fillBag(0, size)
+        dp = [[0] * (size + 1) for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            for j in range(1, size + 1):
+                dp[i][j] = dp[i - 1][j]
+                if(j >= weight[i - 1]):
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - weight[i - 1]] + value[i - 1])
+        return dp[n][size]
             
 
 
